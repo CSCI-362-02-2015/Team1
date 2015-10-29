3bf304cf58720c303a3b957a110597f57e773d4c
Daniel Hurlburt <Daniel@Daniels-MacBook-Pro.local>
2015-10-29 12:51:29 -0400

    test

--- TestAutomation/project/openmrs-core/api/src/main/java/org/openmrs/Test.java
+++ TestAutomation/project/openmrs-core/api/src/main/java/org/openmrs/Test.java
@@ -10,16 +10,16 @@ package org.openmrs;
  * @author daniel
  */
 public class Test {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Integer[] arrA = new Integer[] {1,2,3,4};
-        String strA = new String("a");
-        Cohort cohortA = new Cohort(strA, strA, arrA);
-        System.out.println("success");
-    }
-    
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		Integer[] arrA = new Integer[] { 1, 2, 3, 4 };
+		String strA = new String("a");
+		Cohort cohortA = new Cohort(strA, strA, arrA);
+		System.out.println("success");
+	}
+	
 }
