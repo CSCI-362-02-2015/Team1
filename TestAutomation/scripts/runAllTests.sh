d042059690d7c9f04af62cbaf25af6d19928af46
Daniel Hurlburt <DanielW9818@gmail.com>
2015-11-07 12:00:27 -0500

    Test Automation

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -15,7 +15,8 @@ numTest=$(ls | wc -l)		#count the number of files in the directory
 
 #begin the new report
 cd ../reports
-echo "<html>" >> results.txt	
+echo "<html>" >> results.txt
+echo "<h1>Test Results</h1>" >> results.txt	
 
 #cd ../testCaseExecutables
 #javac -classpath "../project/openmrs-core/api/target/classes" CohortUnionDriver.java
@@ -26,30 +27,33 @@ while [ $num -le $numTest ]; do
 	while read p; do
 	    if [[ ${p:0:1} == D* ]];
 			then result=$(java -cp ../testCaseExecutables/:../project/openmrs-core/api/target/classes/org/openmrs ${p:8} "$input" "$output"); 	#store any System.out from the driver as result and put in the report
-			echo "Test: "$result >> ../reports/results.txt
+			#echo "Test: "$result >> ../reports/results.txt
+			if [[ $result == Passed ]];
+				then echo '<font color="green">Test: '$result' </font><br>' >> ../reports/results.txt;
+			fi 
 			echo $num $result
 		fi
 
 		if [[ ${p:0:1} == T* ]];
-			then echo $p >> ../reports/results.txt;		#put the various test case information into the report
+			then echo $p'<br>' >> ../reports/results.txt;		#put the various test case information into the report
 		fi
 
 		if [[ ${p:0:1} == R* ]];
-			then echo $p >> ../reports/results.txt;
+			then echo $p'<br>' >> ../reports/results.txt;
 		fi
 
 		if [[ ${p:0:1} == I* ]];
-			then echo $p >> ../reports/results.txt;
+			then echo $p'<br>' >> ../reports/results.txt;
 			input=$p
 		fi
 
 		if [[ ${p:0:1} == E* ]];
-			then echo $p >> ../reports/results.txt;
+			then echo $p'<br>' >> ../reports/results.txt;
 			output=$p
 		fi
 	done <$var
 	num=$((num+1))
-	echo " " >> ../reports/results.txt
+	echo "<br>" >> ../reports/results.txt
 done
 cd ../reports
 echo "</html>" >> results.txt
