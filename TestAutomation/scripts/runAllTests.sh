4802a810e73ceec21dc9fd2cfef0d2a02c06870d
Daniel Hurlburt <DanielW9818@gmail.com>
2015-12-05 18:01:11 -0500

    Test Automation

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 
 #remove old reports
 cd reports
-rm results.html
+rm testReport.html
 
 
 cd ../testCases
@@ -17,10 +17,10 @@ numTest=$(ls | wc -l)		#count the number of files in the directory
 
 #begin the new report
 cd ../reports
-echo "<html>" >> results.html
-echo '<link rel="stylesheet" type="text/css" href="report.css">' >> results.html
-echo "<h1>Test Results</h1>" >> results.html
-echo "<table style='border: 1px solid black; border-collapse: collapse'><tr><th>TestID</th><th>Requirement</th><th>Inputs</th><th>Expected Output</th><th>Test Result</th></tr>" >> results.html
+echo "<html>" >> testReport.html
+echo '<link rel="stylesheet" type="text/css" href="report.css">' >> testReport.html
+echo "<h1>Test Results</h1>" >> testReport.html
+echo "<table style='border: 1px solid black; border-collapse: collapse'><tr><th>TestID</th><th>Requirement</th><th>Inputs</th><th>Expected Output</th><th>Test Result</th></tr>" >> testReport.html
 
 
 cd ../project/openmrs-core/api/src/main/java/org/openmrs
@@ -32,45 +32,45 @@ javac -cp ../project/openmrs-core/api/target/classes/org/openmrs *.java
 #loop through the test cases
 while [ $num -le $numTest ]; do
 	var=$str$num$ext
-	echo '<tr>' >> ../reports/results.html
+	echo '<tr>' >> ../reports/testReport.html
 	while read p; do
 	    if [[ ${p:0:1} == D* ]];
 			then result=$(java -cp ../testCaseExecutables/:../project/openmrs-core/api/target/classes/org/openmrs ${p:8} "$input" "$output"); 	#store any System.out from the driver as result and put in the report
 			if [[ $result == Passed ]];
-				then echo '<th><font color="green"> '$result' </font></th>' >> ../reports/results.html;
+				then echo '<th><font color="green"> '$result' </font></th>' >> ../reports/testReport.html;
 				echo -e Test $num ${GREEN}$result${NC}
 			fi 
 			
 			if [[ $result == Failed ]];
-				then echo '<th><font color="red"> '$result' </font></th>' >> ../reports/results.html;
+				then echo '<th><font color="red"> '$result' </font></th>' >> ../reports/testReport.html;
 				echo -e Test $num ${RED}$result${NC}
 			fi 
 
 		fi
 
 		if [[ ${p:0:1} == T* ]];
-			then echo '<th>'${p:8}'</th>' >> ../reports/results.html;		#put the various test case information into the report
+			then echo '<th>'${p:8}'</th>' >> ../reports/testReport.html;		#put the various test case information into the report
 		fi
 
 		if [[ ${p:0:1} == R* ]];
-			then echo '<th>'${p:12}'</th>' >> ../reports/results.html;
+			then echo '<th>'${p:12}'</th>' >> ../reports/testReport.html;
 		fi
 
 		if [[ ${p:0:1} == I* ]];
-			then echo '<th>'${p:8}'</th>' >> ../reports/results.html;
+			then echo '<th>'${p:8}'</th>' >> ../reports/testReport.html;
 			input=$p
 		fi
 
 		if [[ ${p:0:1} == E* ]];
-			then echo '<th>'${p:10}'</th>' >> ../reports/results.html;
+			then echo '<th>'${p:10}'</th>' >> ../reports/testReport.html;
 			output=$p
 		fi
 	done <$var
 	num=$((num+1))
-	echo "</tr>" >> ../reports/results.html
+	echo "</tr>" >> ../reports/testReport.html
 done
 cd ../reports
-echo "</table>" >> results.html
-echo "</html>" >> results.html
+echo "</table>" >> testReport.html
+echo "</html>" >> testReport.html
 
-xdg-open results.html
+xdg-open testReport.html
